aa8145beeb0f2afc8a9f3a55ee0fc5d3b3776af5
Florian Wieland <fwieland@spirit21.com>
2020-11-20 19:38:11 +0100

    Initial Commit: Angular Project Setup and Basic structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: LandingComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
